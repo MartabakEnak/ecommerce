d5585626e4463d80058966865d1ec748bda62105
OkaAnanda <okanandaa007@gmail.com>
2025-07-25 01:34:10 +0800

    p

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-col gap-4">
                     {{ __("You're logged in!") }}
+                    <form method="GET" action="{{ route('logout') }}">
+                        <button type="submit" class="mt-4 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition">
+                            Logout
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
